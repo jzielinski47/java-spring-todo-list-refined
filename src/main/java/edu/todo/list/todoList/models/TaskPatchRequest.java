9df1d50952d4b6c29420f334a5454e2beb1b7191
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-03-30 20:32:34 +0200

    Updated DTO restrictions

--- src/main/java/edu/todo/list/todoList/models/TaskPatchRequest.java
+++ src/main/java/edu/todo/list/todoList/models/TaskPatchRequest.java
@@ -1,9 +1,16 @@
 package edu.todo.list.todoList.models;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public class TaskPatchRequest {
+    @NotBlank(message = "Name cannot be empty")
     private String name;
-    private Integer priority;
-    private Boolean completed;
+    @Min(value = 0, message = "Minimum priority is 0")
+    @Max(value = 5, message = "Maximum priority is 5")
+    private int priority = 0;
+    private boolean completed = false;
 
     public String getName() {
         return name;
